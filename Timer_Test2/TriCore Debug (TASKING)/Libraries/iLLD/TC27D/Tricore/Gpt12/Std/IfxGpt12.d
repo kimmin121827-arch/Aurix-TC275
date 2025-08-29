58ac76722efad240544b224679e348df438cd018
kimmin <kimmin121827@gmail.com>
2025-08-29 16:48:15 +0900

    Add Aurix TC275 project files to temp-branch

---
+++ Timer_Test2/TriCore Debug (TASKING)/Libraries/iLLD/TC27D/Tricore/Gpt12/Std/IfxGpt12.d
@@ -0,0 +1,102 @@
+IfxGpt12.o :	../Libraries/iLLD/TC27D/Tricore/Gpt12/Std/IfxGpt12.c
+../Libraries/iLLD/TC27D/Tricore/Gpt12/Std/IfxGpt12.c :
+IfxGpt12.o :	..\Libraries\iLLD\TC27D\Tricore\Gpt12\Std\IfxGpt12.h
+..\Libraries\iLLD\TC27D\Tricore\Gpt12\Std\IfxGpt12.h :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxGpt12_cfg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxGpt12_cfg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Src\Std\IfxSrc.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Src\Std\IfxSrc.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxSrc_cfg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxSrc_cfg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Configurations\Ifx_Cfg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Configurations\Ifx_Cfg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+IfxGpt12.o :	"C:\Infineon\AURIX-Studio-1.10.16\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.16\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\Ifx_TypesReg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\Ifx_TypesReg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxScu_cfg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxScu_cfg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_bf.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_bf.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_bf.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_bf.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.asm.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.asm.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxCpu_cfg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxCpu_cfg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxScu_PinMap.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxScu_PinMap.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxPort_cfg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxPort_cfg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\Ifx_PinMap.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\Ifx_PinMap.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxGpt12_reg.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxGpt12_reg.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxGpt12_regdef.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxGpt12_regdef.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxGpt12_PinMap.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxGpt12_PinMap.h" :
+IfxGpt12.o :	"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h"
+"D:\\Project\\Aurix-TC275\\Timer_Test2\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h" :
